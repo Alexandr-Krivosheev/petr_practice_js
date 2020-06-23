acacd5d1963716f4644c7a7146a013318710e630
Alexandr <cpt.trustme@gmail.com>
2020-06-23 16:31:51 +0300

    The use of conditions and loops

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,12 +9,42 @@ const personalMovieDB = {
     genres: [],
     privat: false,
 };
-const firstFilm = prompt('Один из последних просмотренных фильмов?','');
-const firstEstimation = prompt('На сколько оцените его?','');
-const secondFilm = prompt('Один из последних просмотренных фильмов?','');
-const secondEstimation = prompt('На сколько оцените его?','');
+for (let i = 0; i < 2; i++) {
+    const firstFilm = prompt('Один из последних просмотренных фильмов?', '');
+    const firstEstimation = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[firstFilm] = firstEstimation;
-personalMovieDB.movies[secondFilm] = secondEstimation;
+    if(firstFilm !== null && firstEstimation !== null && firstFilm !== '' && firstEstimation !== '' && firstFilm.length < 50){
+        personalMovieDB.movies[firstFilm] = firstEstimation;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+// let num = 0;
+// while(num < 2){
+//     const a = prompt('Один из последних просмотренных фильмов?', '');
+//     const b = prompt('На сколько оцените его?', '');
+//
+//     if(a !== null && b !== null && a !== '' && b !== '' && a.length < 50){
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     }else {
+//         console.log('error');
+//         num--;
+//     }
+//     num++;
+// }
+
+if(personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log()
+}else if (personalMovieDB.count > 30){
+    console.log('Вы киноман');
+} else {
+    console.log('error');
+}
+
 
 console.log(personalMovieDB);
