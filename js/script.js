a3565d344597ee69112df4b3389a30a9618a9aae
Alexandr <cpt.trustme@gmail.com>
2020-06-23 15:31:59 +0300

    Start creating an app

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const firstFilm = prompt('Один из последних просмотренных фильмов?','');
+const firstEstimation = prompt('На сколько оцените его?','');
+const secondFilm = prompt('Один из последних просмотренных фильмов?','');
+const secondEstimation = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[firstFilm] = firstEstimation;
+personalMovieDB.movies[secondFilm] = secondEstimation;
+
+console.log(personalMovieDB);
